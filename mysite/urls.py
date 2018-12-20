c848ad8e66265b58841d99821b35647765ee7237
AnnaRodionova <aarodi101@gmail.com>
2018-12-20 21:50:04 +0300

    add visible comments

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,6 +22,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/login/$', views.login, name='login'),
     url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
-    url(r'', include('blog.urls')),url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
 ]
